62f1d59cea28e042d0ebd755b72921b9d3507059
tinko <kini.mesi@gmail.com>
2014-12-27 20:44:42 +0200

    added docstring

--- FrequencyTransformer.py
+++ FrequencyTransformer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-__author__ = 'ilkin safarli'
+__author__ = 'tinko minko'
 
 import numpy as np
 import pickle
@@ -7,16 +7,30 @@ from scipy import sparse
 
 
 class FrequencyTransformer(object):
+	"""
+	This is a custom transformer for scikit-learn: creates word frequency array for each document.
+	"""
 	def __init__(self, count_vect):
+		"""
+		:param count_vect: vector counter object
+		"""
 		self.word_freq = pickle.load(open("word_freq.db", "rb"))
 		self.count_vect = count_vect
 
-	def fit(self, x = None):
+	def fit(self, x = None, y = None):
+		"""
+		This transformer does not need to be fitted, hence return self
+		"""
 		return self
 
-	def transform(self, arr):
-		array = []
-		for doc in arr:
+	def transform(self, array):
+		"""
+		This function creates frequency array.
+		:param array: an array where rows are documents and columns are word counts
+		:return: frequency matrix
+		"""
+		freq_array = []
+		for doc in array:
 			tmp = []
 			word_tfidf = dict(zip(self.count_vect.get_feature_names(), doc))
 			for word in word_tfidf:
@@ -24,6 +38,6 @@ class FrequencyTransformer(object):
 					tmp.append(self.word_freq[word])
 				else:
 					tmp.append(0)
-			array.append(tmp)
-		B = np.array(array)
+			freq_array.append(tmp)
+		B = np.array(freq_array)
 		return sparse.csr_matrix(B)
